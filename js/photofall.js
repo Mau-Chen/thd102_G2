641b147481231ecd955c971d0bfc3e3a9e671457
weicheng000 <135691357+weicheng000@users.noreply.github.com>
2023-08-16 01:54:46 +0800

    修改代碼使子元件能正確接收到父元件的data,未來所有頁面的父元件將合併，專門用來收資料庫的資料

--- js/photofall.js
+++ js/photofall.js
@@ -1,5 +1,52 @@
 import photofall from './waterfall.js';
 
-const app = Vue.createApp({});
-app.component("PhotoFall", photofall);
-app.mount('#photofall');
+const app2 = Vue.createApp({
+    data(){
+        return {
+            items: [
+                { 
+                  image: '../images/pic/photoWall/photoWall01.png', 
+                  text: '項目 1' 
+                },
+                { 
+                  image: '../images/pic/photoWall/photoWall02.png', 
+                  text: '項目 2' 
+                },
+                { 
+                  image: '../images/pic/photoWall/photoWall03.png', 
+                  text: '項目 3' 
+                },
+                { 
+                  image: '../images/pic/photoWall/photoWall04.png', 
+                  text: '項目 4' 
+                },
+                { 
+                  image: '../images/pic/photoWall/photoWall05.png', 
+                  text: '項目 5' 
+                },
+                { 
+                  image: '../images/pic/photoWall/photoWall06.png', 
+                  text: '項目 6' 
+                },
+                { 
+                  image: '../images/pic/photoWall/photoWall07.png', 
+                  text: '項目 7' 
+                },
+                { 
+                  image: '../images/pic/photoWall/photoWall08.png', 
+                  text: '項目 8' 
+                },
+                { 
+                  image: '../images/pic/photoWall/photoWall09.png', 
+                  text: '項目 9' 
+                },
+                { 
+                  image: '../images/pic/photoWall/photoWall10.png', 
+                  text: '項目 10' 
+                }
+              ]
+        }
+    }
+});
+app2.component("PhotoFall", photofall);
+app2.mount('#photofall');
