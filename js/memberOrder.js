3a232be0e8176db184127075b40267b5b2a4b3ff
Ashiyeon <oopschristian@gmail.com>
2023-08-17 18:35:28 +0800

    amed memberOrder  accordion @ashi

--- js/memberOrder.js
+++ js/memberOrder.js
@@ -5,7 +5,7 @@ const app = Vue.createApp({
     return {
       tasks: [
         {
-          OrderId: "PT0004",
+          OrderId: "PT0003",
           OrderDate: "2023年8月4日",
           reduce: 30,
           OrderList: [
@@ -33,7 +33,7 @@ const app = Vue.createApp({
           ],
         },
         {
-          OrderId: "PT0003",
+          OrderId: "PT0004",
           OrderDate: "2023年7月30日",
           reduce: 30,
           OrderList: [
@@ -60,26 +60,63 @@ const app = Vue.createApp({
             },
           ],
         },
-        // {
-        //   OrderId: "PT0002",
-        //   OrderDate: "2023年7月24日",
-        //   reduce: 30,
-        //   OrderList: [
-        //     {
-        //       price: "300",
-        //     },
-        //   ],
-        // },
-        // {
-        //   OrderId: "PT0001",
-        //   OrderDate: "2023年5月31日",
-        //   reduce: 30,
-        //   OrderList: [
-        //     {
-        //       price: "1200",
-        //     },
-        //   ],
-        // },
+        {
+          OrderId: "PT0005",
+          OrderDate: "2023年7月29日",
+          reduce: 30,
+          OrderList: [
+            {
+              name: "寵物接送",
+              image: "./images/pic/shop/goShop01.png",
+              price: 600,
+              date: "8月5日 | 15:00",
+              product: "轎車"
+            },
+            {
+              name: "快樂寵物旅館",
+              image: "./images/pic/shop/goShop02.png",
+              price: 4000,
+              date: "8月5日-8月10日(5晚)",
+              product: "狗套房X1 | 小型犬"
+            },
+            {
+              name: "快樂寵物旅館",
+              image: "./images/pic/shop/goShop03.png",
+              price: 4000,
+              date: "8月5日-8月10日(5晚)",
+              product: "貓套房X1 | 貓咪"
+            },
+          ],
+        },
+        {
+          OrderId: "PT0006",
+          OrderDate: "2023年7月26日",
+          reduce: 30,
+          OrderList: [
+            {
+              name: "寵物接送",
+              image: "./images/pic/shop/goShop01.png",
+              price: 300,
+              date: "8月2日 | 14:00",
+              product: "轎車"
+            },
+            {
+              name: "快樂寵物旅館",
+              image: "./images/pic/shop/goShop02.png",
+              price: 3200,
+              date: "8月2日-8月4日(2晚)",
+              product: "狗套房X2 | 小型犬"
+            },
+            {
+              name: "快樂寵物旅館",
+              image: "./images/pic/shop/goShop03.png",
+              price: 1600,
+              date: "8月2日-8月4日(2晚)",
+              product: "貓套房X1 | 貓咪"
+            },
+          ],
+        },
+
       ],
     };
   },
