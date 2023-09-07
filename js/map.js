72d33bf77248f0cbc4979d69e2a11dc0c3700029
Pineapple <ccc585858@gmail.com>
2023-09-07 23:42:48 +0800

    google map fix @pineapple

--- js/map.js
+++ js/map.js
@@ -2,88 +2,62 @@
 
 // import DriverSelector from "./js/components/DriverSelector.js";
 // console.log(window.VueUse);
+// google map route
 let map, directionsService, directionsRenderer;
+
 async function calculateDistance() {
-  const originInput = document.getElementById("start_place_input");
-  const originInput_value = originInput.value;
-  // console.log(originInput_value);
-  // console.log(originInput);
-  const destinationInput = document.getElementById("end_place_input");
-  const destinationInput_value = destinationInput.value;
+  try {
+    const originInput = document.getElementById("start_place_input");
+    const originInput_value = originInput.value;
+    const destinationInput = document.getElementById("end_place_input");
+    const destinationInput_value = destinationInput.value;
 
-  // 使用地點名稱來取得座標
-  const geocoder = new google.maps.Geocoder();
-  const originCoordinates = await getCoordinates(geocoder, originInput_value);
-  const destinationCoordinates = await getCoordinates(
-    geocoder,
-    destinationInput_value
-  );
+    const geocoder = new google.maps.Geocoder();
+    const originCoordinates = await getCoordinates(geocoder, originInput_value);
+    const destinationCoordinates = await getCoordinates(
+      geocoder,
+      destinationInput_value
+    );
 
-  // originInput.addEventListener("keyup", function (event) {
-  //   if (event.key === "Enter") {
-  //     calculateDistance();
-  //   }
-  // });
-  // destinationInput.addEventListener("keyup", function (event) {
-  //   if (event.key === "Enter") {
-  //     calculateDistance();
-  //   }
-  // });
+    if (originCoordinates && destinationCoordinates) {
+      const request = {
+        origin: originCoordinates,
+        destination: destinationCoordinates,
+        travelMode: "DRIVING",
+      };
 
-  if (originCoordinates && destinationCoordinates) {
-    const request = {
-      origin: originCoordinates,
-      destination: destinationCoordinates,
-      travelMode: "DRIVING",
-    };
-
-    directionsService.route(request, (response, status) => {
-      if (status === "OK") {
-        // console.log(window.vue_app);
-
-        const vm = window.vue_app._instance.data;
-        // console.log(vm.car_menu);
-        // console.log(vm.cars_data[vm.car_menu].cost);
-        directionsRenderer.setDirections(response);
-        const distance = response.routes[0].legs[0].distance.text;
-        // console.log(response.routes[0].legs[0].distance);
-        // console.log(response.routes[0].legs[0].distance.value);
-        let distance_value = response.routes[0].legs[0].distance.value / 1000;
-        let roundedDistance = parseFloat(distance_value.toFixed(1));
-        // console.log(roundedDistance);
-        const car_menu_cost = roundedDistance * vm.cars_data[vm.car_menu].cost;
-        // console.log(car_menu_cost);
-        const duration = response.routes[0].legs[0].duration.text; // 取得旅程所需時間
-        document.getElementById("distanceDisplay").textContent = `${distance}`;
-        document.getElementById("durationDisplay").textContent = `${duration}`; // 顯示旅程所需時間
-        document.getElementById(
-          "costDisplay"
-        ).textContent = `NT$ ${car_menu_cost}`;
-
-        // console.log(costDisplay);
-      } else {
-        alert("Directions request failed due to " + status);
-      }
-    });
-  } else {
-    alert("請輸入有效的出發地和目的地");
+      directionsService.route(request, (response, status) => {
+        if (status === "OK") {
+          const vm = window.vue_app._instance.data;
+          directionsRenderer.setDirections(response);
+          const distance = response.routes[0].legs[0].distance.text;
+          let distance_value = response.routes[0].legs[0].distance.value / 1000;
+          let roundedDistance = parseFloat(distance_value.toFixed(1));
+          const car_menu_cost =
+            roundedDistance * vm.cars_data[vm.car_menu].cost;
+          const duration = response.routes[0].legs[0].duration.text;
+          document.getElementById(
+            "distanceDisplay"
+          ).textContent = `${distance}`;
+          document.getElementById(
+            "durationDisplay"
+          ).textContent = `${duration}`;
+          document.getElementById(
+            "costDisplay"
+          ).textContent = `NT$ ${car_menu_cost}`;
+        } else {
+          alert("Directions request failed due to " + status);
+        }
+      });
+    } else {
+      alert("請輸入有效的出發地和目的地");
+    }
+  } catch (error) {
+    console.error("An error occurred:", error);
   }
 }
 
 async function getCoordinates(geocoder, location) {
-  // await geocoder.geocode({ address: location }, (results, status) => {
-  //   console.log("rr");
-  //   console.log(results);
-  //   if (status === "OK") {
-  //     return results[0].geometry.location;
-  //     //resolve(results[0].geometry.location);
-  //   } else {
-  //     //reject(null);
-  //     return null;
-  //   }
-  // });
-  // console.log("tt");
-
   return new Promise((resolve, reject) => {
     geocoder.geocode({ address: location }, (results, status) => {
       if (status === "OK") {
@@ -108,11 +82,9 @@ window.onload = async function () {
   directionsRenderer.setMap(map);
 
   const searchButton = document.getElementById("driverbtn");
-  // console.log(searchButton);
   searchButton.addEventListener("click", calculateDistance);
 
-  // 網頁加載完畢後，立即進行地圖搜尋
-  let a = calculateDistance();
+  calculateDistance(); // 網頁加載完畢後，立即進行地圖搜尋
 };
 
 //將資料放在localStorage
